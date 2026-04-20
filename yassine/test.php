5808d97256ea402c634fbf6e17c30071f563b926
Yassine-Elamri34 <elamriyassin34@gmail.com>
2026-04-20 11:17:39 -0700

    Yassine - final version with comments and styling

--- yassine/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "root", "pet_adoption", 3306);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully!";
-}
-?>
