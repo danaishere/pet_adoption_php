5dd3ab2b8e4c14adf701739a14aa674ffd5b60da
shallu <shalluca1009@gmail.com>
2026-04-22 10:53:08 -0400

    remove sql file

---
+++ .history/shared/shelters_20260420141952.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Apr 20, 2026 at 06:19 PM
+-- Server version: 8.0.40
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pet_adoption`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shelters`
+--
+
+CREATE TABLE `shelters` (
+  `shelter_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(150) NOT NULL,
+  `type` enum('Shelter','Rescue','Foster Home') NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL,
+  `capacity` int NOT NULL,
+  `current_occupancy` int DEFAULT '0',
+  `verification_status` enum('Pending','Verified','Rejected') DEFAULT 'Pending',
+  `partnership_status` enum('Active','Inactive') DEFAULT 'Active',
+  `description` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ;
+
+--
+-- Dumping data for table `shelters`
+--
+
+INSERT INTO `shelters` (`shelter_id`, `name`, `type`, `email`, `phone`, `address`, `city`, `province`, `capacity`, `current_occupancy`, `verification_status`, `partnership_status`, `description`, `created_at`) VALUES
+(3, 'Sameera', 'Shelter', 'sameera@gmail.com', '123456789', ' Hudson Drive', 'Brampton', 'ON', 100, 0, 'Pending', 'Active', '0', '2026-04-20 17:38:58'),
+(6, 'Sameera Always Smiles', 'Rescue', 'sameerashines@gmail.com', '1234567', '56 Brampton', 'Brampton', 'ON', 200, 0, 'Pending', 'Active', '0', '2026-04-20 18:05:32');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shelters`
+--
+ALTER TABLE `shelters`
+  ADD PRIMARY KEY (`shelter_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shelters`
+--
+ALTER TABLE `shelters`
+  MODIFY `shelter_id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
